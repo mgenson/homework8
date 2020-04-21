fa12e4b02d107cbbf5fed6bbc798908dfa77e447
mgenson <46972714+mgenson@users.noreply.github.com>
2020-04-20 21:44:55 -0400

    Add files via upload

---
+++ challenge3.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  $("#ch3form").submit(fucntion(){
+    $("#input[name='standing']").change(function(){
+      if (this.checked){
+        
+      }
+    })
+  })
+})
+
+
+
+
+
+// $(document).ready(function(){
+//   var grade = false;
+//   var year = false;
+//   $("input[type='submit']").click(function(e){
+//     if (grade == true && year ==false){
+//     }
+//     if(grade == false && year ==true){
+//
+//     }
+//     if (grade == false & year ==false){
+//
+//     }
+//   })
+//   $("input[name='grade']").click(function(){
+//     grade = true;
+//     console.log(this.checked);
+//     console.log("yes, i'm checked");
+//   })
+//   $("input[name='year']").click(function(){
+//     standing = true;
+//     console.log(this.checked);
+//     console.log("Yes, i'm checked");
+//   })
+// })
